6533d6d21b52e5bfe8d624bdb9e108bfc13a149b
Ankitkm95 <ankitmishra517@gmail.com>
2023-05-12 15:53:04 +0530

    ankit_day_1

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config(process.env.MONGOURL);
 
 const connection = mongoose.connect(process.env.MONGOURL);
 
 
 module.exports = connection;
+
+// process.env.MONGOURL
+
+// "mongodb://127.0.0.1:27017/health"
